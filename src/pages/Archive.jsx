6d8aa7e00fdf2ea3635bc909c9094606eda31ed5
Alexandre Tilly <agtaz19@gmail.com>
2026-08-03 23:02:45 -0400

    altered images

--- src/pages/Archive.jsx
+++ src/pages/Archive.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import ArchiveImage from "https://images.pexels.com/photos/18343396/pexels-photo-18343396.jpeg";
+//import ArchiveImage from "https://images.pexels.com/photos/18343396/pexels-photo-18343396.jpeg";
 import AlienAttack from "@/components/arcade/AlienAttack.jsx";
 import Blackjack from "@/components/arcade/Blackjack.jsx";
 import Dropper from "@/components/arcade/BLockDropper.jsx";
@@ -173,7 +173,7 @@ export default function Archive() {
             <section className="relative bg-dark-panel overflow-hidden">
                 <div className="absolute inset-0">
                     <img
-                    src={ArchiveImage}
+                    src={"https://images.pexels.com/photos/18343396/pexels-photo-18343396.jpeg"}
                         alt=""
                         className="w-full h-full object-cover object-center opacity-50"
                     />
